c66f194357b80245b8954f5e124a72ff473ffa88
ezeugwu romanus <romeobourne211@gmail.com>
2022-06-06 21:27:39 +0100

    first upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// function addUser(name, age){
+//  console.log(name +' '+ 'ìs' + age)
+// }
+// addUser('daniel',23);
+let daniel = document.getElementById("daniel");
+
+
+
+function danny() {
+    
+    
+  }
+  daniel.addEventListener("input", danny);
+  
